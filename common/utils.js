25dd5a18933a5552f7a5b81b4f30b8687f5ca891
lemao-code <th.ferrari@outlook.com.br>
2021-09-29 13:01:40 -0300

    Add utils => Class validateFieldsBodyMotorista

---
+++ common/utils.js
@@ -0,0 +1,28 @@
+class Utils {
+    validateFieldsBodyMotorista(body) {
+
+        const isRequiredParams = ["nome", "cpf", "cnh", "renavam", "placa", "cep", "rua", "numero", "bairro", "password"].sort();
+        const paramsBody = Object.keys(body).sort();
+        let missingFields = [];
+        isRequiredParams.map((param) => {
+            if(paramsBody.indexOf(param) === -1 ) {
+                missingFields.push(param)
+            }
+        })
+        if(missingFields.length > 0) {
+            return  {
+                status: 400,
+                fields: missingFields
+            }
+        }else {
+            return {
+                status: 200
+            }
+        }
+    }
+}
+
+
+
+
+module.exports = new Utils;
--- src/validations/motorista.js
+++ src/validations/motorista.js
@@ -1,29 +1,33 @@
 const crudMotorista = require('../crud/motorista');
 const Validate = require('../../common/validateCpf');
 const ValidateMotorista = require('../../common/validateMotorista');
+const utils = require('../../common/utils');
 
 
 class Motorista {
     validations(res, data) {
-
-        //validar Veiculo e Motorista F
+    
         if(data.cpf.length > 11) {
             return res.status(400).send('CPF maior que 11 digitos');
         }
-        if(!data.cpf || !data.renavam || !data.cnh || !data.nome || !data.password) {
-            return res.status(400).send('Favor preencher campos do formulário corretamente.');
-        }
-        // Validar CPF e CNH motorista
-        const isValidCPF = Validate.cpf(data.cpf);
-    
-        // Validar Veiculo
-        const isValidCnh = ValidateMotorista.cnh('e2342432432');
-        const isValidVeiculo = ValidateMotorista.veiculo('324098294', 'FAR8990');
+        const isValidParams = utils.validateFieldsBodyMotorista(data);
 
-        if(isValidCPF && isValidCnh && isValidVeiculo) {
-            crudMotorista.signup(res, "data");
-        } else {
-            return res.status(400).send('Dados incorretos');
+        if(isValidParams.status == 200) {
+            const isValidCPF = Validate.cpf(data.cpf);
+            const isValidCnh = ValidateMotorista.cnh('e2342432432');
+            const isValidVeiculo = ValidateMotorista.veiculo('324098294', 'FAR8990');
+
+            if(isValidCPF && isValidCnh && isValidVeiculo) {
+                crudMotorista.signup(res, "data");
+            }else {
+                return res.status(400).send('Dados incorretos');
+            }
+            
+        }else {
+            res.status(400).send({
+                message: 'Campos incompletos',
+                fileds: isValidParams.fields
+            })
         }
     }
 }
